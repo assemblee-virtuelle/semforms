29a5e65956eaa575322a26942cc3cc078cc53cee
jmvanel <jeanmarc.vanel@gmail.com>
2016-01-17 00:32:53 +0100

    FIX "+" button: copy formInteractions.js from semantic_forms

--- public/javascripts/formInteractions.js
+++ public/javascripts/formInteractions.js
@@ -3,7 +3,8 @@
 
 /** button with an action to duplicate the original HTML widget with an empty content */
 function cloneWidget(widgetName) {
-    var existingWidget =  window.document.getElementsByName(widgetName)[0],
+//    var existingWidget =  window.document.getElementsByName(widgetName)[0],
+    var existingWidget =  window.document.getElementById(widgetName),
         addWidget = existingWidget.cloneNode(true),
         parent = existingWidget.parentNode;
     addWidget.value = '';
@@ -14,24 +15,5 @@ function cloneWidget(widgetName) {
 function backlinks(uri) {
     var url = window.document.location.origin + '/backlinks?q=' + encodeURIComponent(uri);
     window.document.location.assign( url );
-}
-
-function backlinks_old(uri) {
-    var req = new XMLHttpRequest(),
-        url = window.document.location.origin + '/backlinks?q=' + encodeURIComponent(uri);
-    req.open('GET', url, false);
-    req.setRequestHeader('Accept', 'text/html');
-    console.log('Sending backlinks HTTP req ' + url);
-    req.send(null);
-    if (req.status === 200) {
-        window.document.documentElement.innerHTML = req.responseText;
-    } else {
-        console.log('req.status ' + req.status);
-        window.alert('backlinks( ' + uri + ') failed: status ' + req.status +
-            '\n\t' + url +
-            '\n\t' + req.statusText +
-            '\n\t' + req.getAllResponseHeaders()
-            );
-    }
 }
 
